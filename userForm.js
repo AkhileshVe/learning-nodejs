205e51baedf511232e3d7b25cc5fd878b92920fe
Akhilesh verma <Akhileshverma207@gmail.com>
2025-11-11 23:58:06 +0530

    formating

--- userForm.js
+++ userForm.js
@@ -1,12 +1,12 @@
-function userformData(req, resp){
-    
-resp.write(
-    `    <form action="/submit" method="post">
+function userformData(req, resp) {
+
+    resp.write(
+        `    <form action="/submit" method="post">
     <input type="text" placeholder="enter name" name="name"/>
     <input type="text" placeholder ="enter email" name= "email" />
     <button>submit</button>
     </form>`
-)
+    )
 }
 
 module.exports = userformData;
