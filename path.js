5e290a0260eb8402214d66d3bc38eab0e9c2d7e2
Akhilesh verma <Akhileshverma207@gmail.com>
2025-11-14 01:07:35 +0530

    path module and global constant

---
+++ path.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const file = "text/peter.txt"
+
+console.log(path.extname(file));
+console.log(path.dirname(file));
+console.log(path.basename(file));
+console.log(path.resolve("text", "peter.txt"));
+console.log(__dirname);
+console.log(__filename);
